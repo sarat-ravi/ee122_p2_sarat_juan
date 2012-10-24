9fba8e5827a9b0c0e6f290fb5216a56832614f08
Sarat Tallamraju <sarattallamraju@gmail.com>
2012-10-24 15:56:25 -0700

    new test case added

--- EE122-Project-2/rip_router.py
+++ EE122-Project-2/rip_router.py
@@ -14,7 +14,7 @@ class RIPRouter (Entity):
          NOTE: Distance Vector D is also a dict (key, val) = (node, cost)
         """
         self.table = {}
-        self.debug = True
+        self.saratdebug = False
 
         # init self as a node with distance vector {}
         self.table[self] = {}
@@ -33,8 +33,8 @@ class RIPRouter (Entity):
         """
         self.routing_table = {}
 
-    def log(self, message, caller="(rip_router)"):
-        if self.debug:
+    def mylog(self, message, caller="(rip_router)"):
+        if self.saratdebug:
             print "%s: %s" %(str(caller), str(message))
 
     def get_distance_vector_of(self, node):
@@ -69,18 +69,18 @@ class RIPRouter (Entity):
 
 
     def get_min_cost_if_sent_to_neighbor_closest_to_dest(self, dest):
-        self.log("get_min_cost called with destination %s" %(str(dest)))
+        self.mylog("get_min_cost called with destination %s" %(str(dest)))
         min_cost = 100 
         my_dv = self.table[self]
         for neighbor, port in self.discovered_nodes.items():
             if neighbor == dest:
-                self.log("\tThe neighbor is the dest")
+                self.mylog("\tThe neighbor is the dest")
                 return self.table[self][neighbor]
 
             neighbor_dv = self.table[neighbor]
             if not dest in neighbor_dv:
                 # neighbor doesn't know how to get to dest, so theres no point
-                self.log("Neighbor %s doesn't know how to get to dest %s" %(str(neighbor), str(dest)))
+                self.mylog("Neighbor %s doesn't know how to get to dest %s" %(str(neighbor), str(dest)))
                 continue
 
             cost_to_neighbor = 1 
@@ -89,7 +89,7 @@ class RIPRouter (Entity):
 
             if cost_to_dest < min_cost:
                 min_cost = cost_to_dest
-                self.log("Added %s : (%s, %s) to routing table" %(str(dest), str(neighbor), str(port)))
+                self.mylog("Added %s : (%s, %s) to routing table" %(str(dest), str(neighbor), str(port)))
                 self.routing_table[dest] = (neighbor, port)
 
         return min_cost
@@ -98,7 +98,7 @@ class RIPRouter (Entity):
     def update_belief(self):
         table_changed = False
 
-        self.log("update belief called")
+        self.mylog("update belief called")
         my_dv = self.table[self]
         for node, distance in my_dv.items():
             if node == self:
@@ -106,32 +106,43 @@ class RIPRouter (Entity):
 
             old_dist = distance 
             new_min_dist = self.get_min_cost_if_sent_to_neighbor_closest_to_dest(dest=node)
-            self.log("Old dist is %s and new_min_dist = %s" %(str(old_dist), str(new_min_dist)))
+            self.mylog("Old dist is %s and new_min_dist = %s" %(str(old_dist), str(new_min_dist)))
             if not old_dist == new_min_dist:
-                self.log("\tDist changed from %s to %s for neighbor %s" %(str(old_dist), str(new_min_dist), str(node)))
+                self.mylog("\tDist changed from %s to %s for neighbor %s" %(str(old_dist), str(new_min_dist), str(node)))
                 table_changed = True
 
             self.table[self][node] = new_min_dist 
 
         return table_changed
 
-    def add_new_possible_destinations_to_my_dv(self, new_dv):
+    def handle_new_destinations_and_withdrawals(self, source, new_dv):
+        # handle new destinations
         for possible_destination, distance in new_dv.items():
             # add this new reachable destination to my dv
-            self.table[self][possible_destination] = distance
+            if not possible_destination in self.table[self]:
+                self.table[self][possible_destination] = distance
+
+        # handle withdrawal
+        for old_neighbors_believed_destination, distance in self.table[source].items():
+            if not old_neighbors_believed_destination in new_dv:
+                # this means this destination has been withdrawn by source node
+                # so we must make it unreachable by making its cost 100
+                # or by popping. Dont know yet
+                self.table[source][old_neighbors_believed_destination] = 100
+                #self.table[source].pop(old_neighbors_believed_destination)
 
     def add_dv_to_table(self, source, dv):
         """
         Handles dv insertion into table, returns True if table changed or false ow
         """
         if source in self.table and self.table[source] == dv:
-            self.log("the old dv %s and new dv %s are the same for %s" %(str(self.table[source]), str(dv), str(source)))
+            self.mylog("the old dv %s and new dv %s are the same for %s" %(str(self.table[source]), str(dv), str(source)))
             return False
         
-        self.log("adding dv '%s' to source '%s'" %(str(dv), str(source)))
+        self.mylog("adding dv '%s' to source '%s'" %(str(dv), str(source)))
         # table changed for sure, at this point
         self.table[source] = dv
-        self.add_new_possible_destinations_to_my_dv(new_dv=dv)
+        self.handle_new_destinations_and_withdrawals(source=source, new_dv=dv)
         table_changed = self.update_belief()
         return table_changed
 
@@ -158,20 +169,20 @@ class RIPRouter (Entity):
 
             if cost_for_neighbor_to_destination < cost_for_me_to_destination:
                 # dont even bother telling. Pretend I can't get to destination
-                self.log("Poisoned neighbor '%s' for destination '%s'" %(str(neighbor), str(destination)))
+                self.mylog("Poisoned neighbor '%s' for destination '%s'" %(str(neighbor), str(destination)))
                 poisoned_dv[destination] = 100
 
         return poisoned_dv
 
     def advertize_my_dv_to_neighbors(self):
-        self.log("called advertize")
+        self.mylog("called advertize")
 
         for neighbor, port in self.discovered_nodes.items():
             if neighbor == self:
                 continue
             if port == None:
                 # the link to this neighbor is down
-                self.log("Port for neighbor %s is %s" %(str(neighbor), str(port)))
+                self.mylog("Port for neighbor %s is %s" %(str(neighbor), str(port)))
                 continue
             
             poisoned_dv = self.get_poisoned_dv_for_neighbor(neighbor=neighbor)
@@ -179,8 +190,8 @@ class RIPRouter (Entity):
             update_packet = RoutingUpdate()
             update_packet.paths = poisoned_dv
 
-            self.log("Advertizing to neighbor: %s" %(str(neighbor)))
-            self.log("Advertizement: " + str(update_packet.paths))
+            self.mylog("Advertizing to neighbor: %s" %(str(neighbor)))
+            self.mylog("Advertizement: " + str(update_packet.paths))
             self.send(packet=update_packet, port=port, flood=False)
 
     def handle_discovery_packet(self, packet, port):
@@ -193,15 +204,19 @@ class RIPRouter (Entity):
             if not neighbor in self.discovered_nodes:
                 # if neighbor is not already there
                 # initialize a dv for our dear neighbor
-                self.log("New neighbor '%s' discovered" %(str(neighbor)))
+                self.mylog("New neighbor '%s' discovered" %(str(neighbor)))
                 self.table[neighbor] = {}
             self.table[self][neighbor] = 1
 
             self.discovered_nodes[neighbor] = port
         else:
-            self.discovered_nodes[neighbor] = None
-            self.table[self][neighbor] = 100 
-            self.log("Link to Neighbor '%s' is down" %(str(neighbor)))
+            #self.discovered_nodes[neighbor] = None
+            #self.table[self][neighbor] = 100 
+
+            # remove neighbor from existance:
+            self.discovered_nodes.pop(neighbor)
+            self.table[self].pop(neighbor)
+            self.mylog("Link to Neighbor '%s' is down" %(str(neighbor)))
 
         table_changed = False
         #table_changed = self.update_belief() 
@@ -214,7 +229,7 @@ class RIPRouter (Entity):
         Get routing table information from someone else, and update table accordingly
         """
         # the distance vector given by the update packet
-        self.log("received update packet: %s" %(str(packet)))
+        self.mylog("received update packet: %s" %(str(packet)))
         dv = packet.paths
         table_changed = self.add_dv_to_table(source=packet.src, dv=dv)
 
@@ -260,17 +275,19 @@ class RIPRouter (Entity):
         #exit_port = self.discovered_nodes[best_neighbor]
         #return exit_port
 
+        if not dest in self.routing_table:
+            return None, None
         best_neighbor, exit_port = self.routing_table[dest]
-
+        return best_neighbor, exit_port
 
     def handle_rx (self, packet, port):
 
-        self.log("----------------------------------------------------------------")
-        self.log("I am %s" %(str(self)))
+        self.mylog("----------------------------------------------------------------")
+        self.mylog("I am %s" %(str(self)))
 
         is_discovery_packet, is_routing_update = self.identify_packet(packet=packet)
         dest, ttl = packet.dst, packet.ttl
-        self.log("Packet Arrived: %s, trying to go to %s" %(str(packet), str(dest)))
+        self.mylog("Packet Arrived: %s, trying to go to %s" %(str(packet), str(dest)))
 
         if is_discovery_packet:
             self.handle_discovery_packet(packet=packet, port=port)
@@ -283,16 +300,20 @@ class RIPRouter (Entity):
 
             if dest == self or dest == None:
                 # hand the packet over to the upper layer
-                self.log("Packet sent for processing")
+                self.mylog("Packet sent for processing")
                 self.send(packet=packet)
 
             if ttl == 0:
                 # packet dropped lol
-                self.log("Packet dropped because ttl is 0")
+                self.mylog("Packet dropped because ttl is 0")
                 return
 
             best_neighbor, exit_port = self.lookup_exit_port_for(dest=dest)
-            self.log("Sending packet to %s" %(str(dest)))
+            if not exit_port:
+                # packet dropped lol
+                return
+
+            self.mylog("Sending packet to %s" %(str(dest)))
             self.send(packet=packet, port=exit_port, flood=False)
 
 
---
+++ EE122-Project-2/tests/awesome_test.py
@@ -0,0 +1,349 @@
+#!/bin/env python
+
+import sys
+sys.path.append('.')
+
+from sim.api import *
+from sim.basics import *
+from rip_router import RIPRouter
+import sim.topo as topo
+import os
+import time
+
+testing = True
+failed = False
+waittime = 5
+expectedUpdate1 = {}
+expectedSender1 = {}
+
+class FakeEntity (Entity):
+    def __init__(self, expectedUpdate, expectedSender, to_announce):
+        self.expectedUpdate = expectedUpdate
+        self.expectedSender = expectedSender
+        self.announce = to_announce
+        self.num_rx = 0
+        #if(self.announce):
+        #    self.timer = create_timer(5, self.send_announce)
+
+    def handle_rx(self, packet, port):
+        global failed
+        global testing
+        if(self.expectedUpdate):
+            if(isinstance(packet, RoutingUpdate)):
+                self.num_rx += 1
+                if not testing:
+                    print "PACKET! "+str(packet.src)
+                    if len(packet.paths) == 0:
+                        print "  EMPTY!"
+                    for dest, cost in packet.paths.iteritems():
+                        print "  "+str(dest)+","+str(cost)
+                    return
+                print "Packet received: "+str(self.num_rx)
+                if not self.expectedUpdate.has_key(self.num_rx):
+                    print "FAILED: extra packet sent by "+str(packet.src)
+                    failed = True
+                elif (packet.src != self.expectedSender[self.num_rx]):
+                    print "FAILED: Sent by "+str(packet.src)+", should be "+str(self.expectedSender[self.num_rx])
+                    failed = True
+                elif len(packet.paths) > len(self.expectedUpdate[self.num_rx]):
+                    print "FAILED: Too many items in packet: "+str(packet.paths)+", should be "+str(self.expectedUpdate[self.num_rx])
+                    failed = True
+                elif len(packet.paths) < len(self.expectedUpdate[self.num_rx]):
+                    print "FAILED: Too few items in packet: "+str(packet.paths)+", should be "+str(self.expectedUpdate[self.num_rx])
+                    failed = True
+                else:
+                    for dest,cost in packet.paths.iteritems():
+                        if dest not in self.expectedUpdate[self.num_rx].keys():
+                            print "FAILED: destination "+str(dest)+" not expected to be in packet."
+                            failed = True
+                        elif cost != self.expectedUpdate[self.num_rx][dest]:
+                            print "FAILED: cost "+str(cost)+" for destination "+str(dest)+" incorrect, should be "+str(self.expectedUpdate[self.num_rx][dest])
+                            failed = True
+
+                  #if dest not in self.expectedUpdate.keys():
+                  #  failed = True
+                  #elif cost != self.expectedUpdate[dest]:
+                  #  failed = True
+
+    def send_announce(self):
+        pass
+        #if(self.announce):
+        #    update = RoutingUpdate()
+        #    for dest, cost in self.announce.iteritems():
+        #      update.add_destination(dest, cost)
+        #    self.send(update, flood=True)
+
+    def send_specific_announce(self, announce):
+        if(announce):
+            update = RoutingUpdate()
+            for dest, cost in announce.iteritems():
+                update.add_destination(dest, cost)
+            self.send(update, flood=True)
+
+def create (switch_type = FakeEntity, host_type = FakeEntity, n = 2):
+    global expectedUpdate1
+    global expectedSender1
+    global waittime
+
+    print "Creating routers..."
+    RIPRouter.create('A')
+    RIPRouter.create('B')
+    RIPRouter.create('C')
+    RIPRouter.create('D')
+    RIPRouter.create('E')
+    RIPRouter.create('F')
+    RIPRouter.create('G')
+    RIPRouter.create('H')
+    RIPRouter.create('I')
+    RIPRouter.create('J')
+    createExpectedDict()
+    FakeEntity.create('Z', expectedUpdate1, expectedSender1, {})
+
+
+    print "Case 1: A-B-Z"
+    topo.link(A, B)      #A - B - Z
+    topo.link(B, Z)
+    time.sleep(waittime)
+    if(failed):
+        print "Failed case 1"
+        return
+
+    print "Case 2: A B-Z"
+    topo.unlink(A, B)    #A   B - Z
+    time.sleep(waittime)
+    if(failed):
+        print "Failed case 2"
+        return
+
+    print "Case 3: A-B Z"
+    topo.unlink(B, Z)
+    time.sleep(waittime/2)
+    topo.link(A, B)      #A - B   Z
+    time.sleep(waittime)
+    if(failed):
+        print "Failed case 3"
+        return
+
+    print "Case 4: A-B-C-D-E-Z"
+    topo.link(E, Z)      #A - B - C - D - E - Z
+    topo.link(B, C)
+    topo.link(C, D)
+    time.sleep(waittime)
+    topo.link(D, E)
+    time.sleep(waittime)
+    if(failed):
+        print "Failed case 4"
+        return
+
+    print "Case 5: Circle(A,E)-Z"
+    topo.link(E, A)      #A - B - C - D - E - Z
+    time.sleep(waittime) # \_____________/
+    if(failed):
+        print "Failed case 5"
+        return
+
+    print "Case 6: Clique(A,E)-Z"
+    #topo.link(A, B)
+    topo.link(A, C)
+    topo.link(A, D)
+    #topo.link(A, E)
+    #topo.link(B, C)
+    topo.link(B, D)      #        ---A---
+    topo.link(B, E)      #       /  / \  \
+    time.sleep(waittime) #      B--|---|-E -- Z
+    #topo.link(C, D)     #       \ |/ \| /
+    topo.link(C, E)      #        C-----D
+    #topo.link(D, E)     #   Clique
+    time.sleep(waittime)
+    if(failed):
+        print "Failed case 6"
+        return
+
+    print "Case 7: Centralize(A,{B,C,D,E,Z}"
+    #topo.unlink(A, B)
+    #topo.unlink(A, C)
+    #topo.unlink(A, D)
+    #topo.unlink(A, E)
+    topo.unlink(E, Z)
+    topo.unlink(B, C)
+    topo.unlink(B, D)    #           Z
+    topo.unlink(B, E)    #           |
+    topo.unlink(C, D)    #      B -- A -- E
+    topo.unlink(C, E)    #          / \
+    topo.unlink(D, E)    #        C     D
+    time.sleep(waittime)
+    topo.link(A, Z)
+    time.sleep(waittime)
+    if(failed):
+        print "Failed case 7"
+        return
+
+    print "Case 8: Centralize(A,Circle(B-E,Z))"
+    topo.link(B, C)
+    topo.link(C, D)
+    topo.link(D, E)
+    time.sleep(waittime)    #       ---Z---
+    topo.link(E, Z)         #      /   |   \
+    time.sleep(waittime/2)  #     B -- A -- E
+    topo.link(Z, B)         #      \  / \  /
+    time.sleep(waittime)    #       C-----D
+    if(failed):
+        print "Failed case 8"
+        return
+
+    print "Case 9: A-B-Branch(Z, C-D-E, F-G)"
+    topo.unlink(A, Z)
+    topo.unlink(E, Z)
+    topo.unlink(B, Z)
+    #topo.unlink(A, B)
+    topo.unlink(A, C)
+    topo.unlink(A, D)
+    topo.unlink(A, E)
+    topo.unlink(B, C)
+    topo.link(F, G)
+    #print "REM"
+    #topo.unlink(C, D)
+    #topo.unlink(D, E)
+    time.sleep(waittime)
+    topo.link(B, Z)
+    time.sleep(waittime/2)
+    Z.send_specific_announce({F: 1, G: 2, C: 1, D: 2, E: 3})
+    time.sleep(waittime/2)
+    topo.unlink(B, Z)
+    topo.link(Z, C)
+    time.sleep(waittime/2)
+    Z.send_specific_announce({F: 1, G: 2, B: 1, A: 2})
+    time.sleep(waittime/2)   #
+    topo.unlink(C, Z)        #             F - G
+    topo.link(Z, F)          #             |
+    time.sleep(waittime/2)   #     A - B - Z - C - D - E
+    Z.send_specific_announce({B: 1, A: 2, C: 1, D: 2, E: 3})
+    time.sleep(waittime/2)
+    topo.unlink(F, G)
+    Z.send_specific_announce({B: 1, A: 2, C: 1, D: 2, E: 3, G: 100})
+    #print "NIM"
+    time.sleep(waittime/2)
+    topo.link(F, G)
+    time.sleep(waittime)
+    if(failed):
+        print "Failed case 9"
+        return
+
+    print "Case 10: EShaped(ABC,EHD,FGZ)"
+    #topo.unlink(B, Z)
+    #topo.unlink(Z, C)
+    topo.unlink(Z, F)
+    #topo.unlink(A, B)
+    topo.unlink(C, D)
+    topo.unlink(D, E)
+    #topo.unlink(F, G)
+    topo.link(B, C)
+    topo.link(A, E)      #     A ------B------C
+    topo.link(E, H)      #     |
+    topo.link(H, D)      #     E ------H------D
+    topo.link(A, F)      #     |
+    time.sleep(waittime) #     F-------G------Z
+    topo.link(G, Z)
+    time.sleep(waittime/2)
+    topo.disconnect(H)
+    time.sleep(waittime)
+    if(failed):
+        print "Failed case 10"
+        return
+
+    A.remove()
+    B.remove()
+    C.remove()
+    D.remove()
+    E.remove()
+    F.remove()
+    G.remove()
+    H.remove()
+    I.remove()
+    J.remove()
+    Z.remove()
+
+def createExpectedDict():
+    global expectedUpdate1
+    global expectedSender1
+    expectedUpdate1[1] = {A: 1}
+    expectedSender1[1] = B
+
+    expectedUpdate1[2] = {}
+    expectedSender1[2] = B
+
+    expectedUpdate1[3] = {}
+    expectedSender1[3] = E
+    expectedUpdate1[4] = {D: 1}
+    expectedSender1[4] = E
+    expectedUpdate1[5] = {A: 4, B: 3, C: 2, D: 1}
+    expectedSender1[5] = E
+
+    expectedUpdate1[6] = {A: 1, B: 3, C: 2, D: 1}
+    expectedSender1[6] = E
+    expectedUpdate1[7] = {A: 1, B: 2, C: 2, D: 1}
+    expectedSender1[7] = E
+
+    expectedUpdate1[8] = {A: 1, B: 1, C: 2, D: 1}
+    expectedSender1[8] = E
+    expectedUpdate1[9] = {A: 1, B: 1, C: 1, D: 1}
+    expectedSender1[9] = E
+
+    expectedUpdate1[10] = {B: 1, C: 1, D: 1, E: 1}
+    expectedSender1[10] = A
+
+    expectedUpdate1[11] = {A: 1, B: 2, C: 2, D: 1}
+    expectedSender1[11] = E
+    expectedUpdate1[12] = {A: 1, C: 1, D: 2, E: 2}
+    expectedSender1[12] = B
+
+    expectedUpdate1[13] = {A: 1}
+    expectedSender1[13] = B
+    expectedUpdate1[14] = {A: 1, C: 100, D: 100, E: 100, F: 100, G: 100}
+    expectedSender1[14] = B
+    expectedUpdate1[15] = {D: 1, E: 2}
+    expectedSender1[15] = C
+    expectedUpdate1[16] = {A: 100, B: 100, D: 1, E: 2, F: 100, G: 100}
+    expectedSender1[16] = C
+    expectedUpdate1[17] = {G: 1}
+    expectedSender1[17] = F
+    expectedUpdate1[18] = {A: 100, B: 100, C: 100, D: 100, E: 100, G: 1}
+    expectedSender1[18] = F
+    expectedUpdate1[19] = {A: 100, B: 100, C: 100, D: 100, E: 100}
+    expectedSender1[19] = F
+    expectedUpdate1[20] = {A: 100, B: 100, C: 100, D: 100, E: 100, G: 1}
+    expectedSender1[20] = F
+
+    expectedUpdate1[21] = {A: 2, B: 3, C: 4, D: 5, E: 3, F: 1, H: 4}
+    expectedSender1[21] = G
+    expectedUpdate1[22] = {A: 2, B: 3, C: 4, E: 3, F: 1}
+    expectedSender1[22] = G
+    #print str(expectedUpdate1)
+
+import sim.core
+from hub import Hub as switch
+
+import sim.api as api
+import logging
+#api.simlog.setLevel(logging.DEBUG)
+api.simlog.setLevel(logging.WARNING)   #So INFO messages don't show up.
+api.userlog.setLevel(logging.DEBUG)
+
+_DISABLE_CONSOLE_LOG = True
+
+
+start = sim.core.simulate
+start()
+
+create(switch)
+
+
+
+
+
+if(failed):
+    print("You have failed since I got unexpected updates!")
+    os._exit(0)
+else:
+    print("Test is successful!")
+    os._exit(2)
